435e2c24476f49b7b19a8027fafd7dfdc850d0e6
mochluthfix <mochluthfix@gmail.com>
2026-01-02 02:41:38 +0700

    fix: remove webpack config to fully use Turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,8 @@
-
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  webpack(config) {
-  
-    config.externals.push({
-      "utf-8-validate": "commonjs utf-8-validate",
-      bufferutil: "commonjs bufferutil",
-    });
-
-    return config;
-  },
-
-
-  transpilePackages: [
-    "three",
-    "@react-three/fiber",
-    "@react-three/drei",
-    "postprocessing",
-  ],
+ 
 };
 
 export default nextConfig;
