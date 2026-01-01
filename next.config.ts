2f71fc092909ba69ec36b88dca9ba000a87dcf37
mochluthfix <mochluthfix@gmail.com>
2026-01-02 02:32:00 +0700

    fix: fully disable Turbopack and force Webpack for stable three.js build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,30 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  
+  experimental: {
+    turbopack: false,
+  },
+
+  
+  webpack(config) {
+    
+    config.externals.push({
+      "utf-8-validate": "commonjs utf-8-validate",
+      bufferutil: "commonjs bufferutil",
+    });
+
     return config;
   },
+
   
-  transpilePackages: ["three"],
+  transpilePackages: [
+    "three",
+    "@react-three/fiber",
+    "@react-three/drei",
+    "postprocessing",
+  ],
 };
 
 export default nextConfig;
